7fbc4c48e7742f51b55254cf8f9a0a8ebaadc822
ItsNotABugOk <chnoman503@gmail.com>
2021-10-13 06:26:37 -0400

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+console.log("JavaScript is amazing!");
+function myFunction() {
+    window.prompt("sometext","defaultText");
+    alert("I am an alert box!");
+}
